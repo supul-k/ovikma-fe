b3f35df47b2adcae68eed800e47107dfa5edfb8e
Supul Kalhara <supulkalhara7@gmail.com>
2023-06-28 18:10:24 +0530

    navbar created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from '../components/Home/Navbar';
+
+const useScrollVisibility = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+  
+    useEffect(() => {
+  
+      const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const isVisible = prevScrollPos > currentScrollPos;
+  
+        setPrevScrollPos(currentScrollPos);
+        setVisible(isVisible);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [prevScrollPos]);
+  
+    return visible;
+  };
+
 const Home = () => {
+
+const visible = useScrollVisibility();
 
   return (
     <div>
-     Hello Home
+      <Navbar visible={visible} />
     </div>
   );
 };
